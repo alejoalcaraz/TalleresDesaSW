21553cc728671f052c37f69f15306e2dd156037f
Alejandro Alcaraz <a.alcaraz@uniandes.edu.co>
2021-04-13 01:41:07 -0500

    Taller1 Typescript

---
+++ taller1Typescript/dataCourses.ts
@@ -0,0 +1,11 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW", "Rubby Casallas", 3),
+  new Course("SisTrans", "Claudia", 3),
+  new Course("TI en las organizaciones", "Oscar Ávila", 3),
+  new Course("Cálculo Vectorial", "Daniel Sanabria", 3),
+  new Course("Fundamentos de Mercadeo", "Silvana Dakduk", 3),
+  new Course("Probabilidad", "Gonzalo Torres", 3),
+  new Course("Análisis de Inversión", "Sergio Umaña", 3)
+]
